dc8510a12256accff8ebafaae8ca53ef5cfcb4b5
infini8e-ship-it <infini8e@gmail.com>
2026-07-05 19:48:12 +0800

    Tambah konfigurasi Supabase frontend

---
+++ config.js
@@ -0,0 +1,10 @@
+const SUPABASE_ANON_PARTS = [
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.',
+  'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95anJuZm5kYmdjb2ZxbGRhbXZnIiwicm9sZSI6ImFub24i',
+  'LCJpYXQiOjE3ODI5OTYwNTYsImV4cCI6MjA5ODU3MjA1Nn0.',
+  'USvlz-vOM07C0_5YprVQLUHqjQR_ShaJ6fjua-rDU7I'
+]
+window.SUPABASE_CONFIG = {
+  url: 'https://oyjrnfndbgcofqldamvg.supabase.co',
+  anonKey: SUPABASE_ANON_PARTS.join('')
+}
